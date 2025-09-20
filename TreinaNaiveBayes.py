bd4eb9959ef83ff3350945e52d9f124f9f30b76c
Maria Luísa Mendonça Oliveira <0049545@academico.ifmg.edu.br>
2025-09-20 17:48:24 -0300

    treina Naive Bayes pronto uhul

--- TreinaNaiveBayes.py
+++ TreinaNaiveBayes.py
@@ -15,12 +15,34 @@ import pandas as pd
 from collections import defaultdict
 import math
 
+from numpy.ma.core import equal
+
+
 def treina_naive_bayes(dataset_treino):
     p_prior = {}
-    p_condicional = {}
+    p_condicional = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
     classe = 9
     classes = [0, 1]
+    atributos = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    valores = [0, 1, 2]
 
     # Probabilidade a priori
-    for c in classes:
+    for c in classes: # Para cada classe existente conta quantas vezes apareceu determinada classe em treino e divide pelo total de dados em treino
         p_prior[c] = len(dataset_treino[dataset_treino[classe] == c]) / len(dataset_treino)
+
+
+    # Probabilidade Condicional
+    for c in classes:
+        # A probabilidade de cada atributo ocorrer dado a classe
+        for a in atributos:
+            for v in valores:
+                # Contando quantos atributos presentes em treino são iguais a cada valor possivel
+                # Dividindo pela quantidade de vezes que a classe ocorreu
+                p_condicional[a][v][c] = len(dataset_treino[(dataset_treino[a] == v) & (dataset_treino[classe] == c)]) / len(dataset_treino[dataset_treino[classe] == c])
+
+    print("Probabilidades a priori (P(c)):\n")
+    for c, p in p_prior.items():
+        print(f"Classe {c}: {p:.4f} ({p * 100:.2f}%)")
+
+    return p_condicional, p_prior
+
